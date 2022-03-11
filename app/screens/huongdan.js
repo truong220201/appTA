7310cc9f810ae052dffeae7837f6302ec4c6863e
truong220201 <91840628+truong220201@users.noreply.github.com>
2022-03-11 12:12:32 +0700

    Update huongdan.js

--- app/screens/huongdan.js
+++ app/screens/huongdan.js
@@ -3,18 +3,185 @@ import { Dimensions,Animated,TouchableOpacity,TouchableHighlight,ScrollView, Tex
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import { LinearGradient } from 'expo-linear-gradient';
 import { Icon } from 'react-native-elements';
-
+import { doc, setDoc } from "firebase/firestore"; 
+import { collection, getDocs } from "firebase/firestore"; 
+import { firebaseApp } from '../../components/firebaseConfig';
+import { getFirestore } from "firebase/firestore";
 
 export default class huongdan extends React.Component{ 
+    constructor(props) {
+        super(props);
+        //this.itemRef = getDatabase(firebaseApp);
+        //console.log(this.itemRef);
+        const { route,navigation } = this.props;
+        this.nvt = navigation;
+        const { baitap,name,n,ten,uid,email} = route.params;
+        const { loaiId,id} = route.params;
+        //console.log('baitap:',id);
+        //console.log('uid form test:',uid)
+
+
+        this.uid=uid;
+        this.email=email;
+        this.i = id;
+        this.detai= ten;
+        this.num = n;
+        this.diems = 0;
+        this.state = {
+            isLoading:true,
+            hideBack:'flex',
+            hideNext:'flex',
+            timer:500,
+            keys:[],
+            item:[],
+            itemQ:1,
+            opt0:[],
+            opt1:[],
+            opt2:[],
+            opt3:[],
+            itemK:[],
+            nameqs:[],
+            leng:0,
+            trueAns:[],
+            //test
+            q:'',
+            a1:'',
+            a2:'',
+            a3:'',
+            a4:'',
+            //
+            selectedOption:'',
+            optList:[],
+            o:[],
+            ans:'',
+            answ:[0,0,0,0,0,0,0,0,0,0],
+        };
+
+        
+        this.opt = this.state.options;
+      }
+
+
+
+    async listenForItemsQS(inum){
+        const db = getFirestore(firebaseApp);
+        const querySnapshotQS = await getDocs(collection(db, "Question"));
+        
+        querySnapshotQS.forEach((doc) => {
+            //console.log(`name qs : ${doc.data().Id_cate_mtct}`);
+            
+            [...Array(this.i.length)].map((o,n) => {
+                if(this.i[n] == `${doc.data().Id_cate_mtct}`){
+                    //console.log('ok');
+                    this.setState({
+                        
+                        itemK:[...this.state.itemK,`${doc.id}`],
+                        nameqs:[...this.state.nameqs,`${doc.data().name_Question}`], 
+                    })
+                    //console.log('length item k: ',this.state.itemK.length);
+                }
+            })
+            
+        })
+        //random itemK va nameqs
+        
+        this.shuffle(this.state.itemK,this.state.nameqs)
+    }
+    
+    async listenForItems(inum){
+        
+        const db = getFirestore(firebaseApp);
+        //console.log(db);
+        //const docRef = doc(db, "Quiz", "03ZnOo7bgWhJvJU9Th9G");
+        //const docSnap = await getDoc(docRef);
+        const querySnapshot = await getDocs(collection(db, "Option"));
+        
+
+        querySnapshot.forEach((doc) => {
+          //console.log(`${doc.id} => ${doc.data().id_Question}`);
+          //console.log('data:',`${doc.data().True_ans}`);
+          //console.log(`length: ${doc.id.length}`);
+         //console.log(this.i);
+         //show length
+         //console.log(this.state.item.length);
+         //console.log('idqs: ',`${doc.data().id_Question}`);
+         //console.log(this.state.itemK.length);
+         [...Array(this.state.itemK.length)].map((o,n) => {
+            if(this.state.itemK[n] == `${doc.data().id_Question}`){
+               //console.log('ok:',this.state.opt0);
+                this.setState({
+                    //item:this.state.item.push(data)
+                    //item:Object.keys(`${doc.data().Title}`)
+                    item:[...this.state.item,`${doc.data().id_Question}`],
+                    //nameqs:[...this.state.nameqs,1],
+                    opt0:[...this.state.opt0,`${doc.data().Option_ans[0]}`],
+                    opt1:[...this.state.opt1,`${doc.data().Option_ans[1]}`],
+                    opt2:[...this.state.opt2,`${doc.data().Option_ans[2]}`],
+                    opt3:[...this.state.opt3,`${doc.data().Option_ans[3]}`],
+                    trueAns:[...this.state.trueAns,`${doc.data().True_ans}`],
+                    isLoading:false,
+                });
+                //console.log('length item k: ',this.state.itemK);
+            }
+        })
+         
+        //console.log(`${doc.data().id_Question}`);
+        //console.log(this.state.itemK[this.num])
+        //console.log('state itemk:' ,this.state.itemK[inum-1]);
+        //console.log('state item:' ,this.state.item.length-1);
+        
+
+        }); 
+        //console.log('item:'+this.state.item);
+        //console.log('length:'+this.state.leng);
+    }
+
+    componentDidMount(){
+        this.listenForItemsQS();
+        this.listenForItems();
+    }
+
+    shuffle(arraya,arrayb) {
+        let currentIndex = arraya.length,  randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+        //itemK:
+        [arraya[currentIndex], arraya[randomIndex]] = [
+            arraya[randomIndex], arraya[currentIndex]];
+        //nameqs
+        [arrayb[currentIndex], arrayb[randomIndex]] = [
+            arrayb[randomIndex], arrayb[currentIndex]];
+        
+        }
+      
+        return arraya,arrayb;
+      }
+
   render(){
     const windowWidth = Dimensions.get('window').width;
     const windowHeight = Dimensions.get('window').height;
     const { navigation,route } = this.props;
-    const { id,name,ten} = route.params;
+    const { loaiId, ten,id,name,uid,email} = route.params;
+    
+    
+    this.uid=uid;
+    this.email=email;
+    //console.log('itemK :',this.state.itemK)
+    
+    //console.log('item :',this.state.item)
+
+
     return (
     <View style={styles.container}>
         <View style={styles.vw1}>
-            <Text style={styles.txtTitle}>Test: {ten}</Text>
+            <Text style={styles.txtTitle}>Quy định làm bài</Text>
         </View>
         <View style={styles.vw2}>
             <View style={styles.yellowView}>
@@ -23,7 +190,7 @@ export default class huongdan extends React.Component{
                 </View>
                 <View style={styles.contentA}>
                     <Text style={styles.txtA}>Thời gian</Text>
-                    <Text style={styles.txtB}>45 Phút</Text>
+                    <Text style={styles.txtB}>15 Phút</Text>
                 </View>
             </View>
             <View style={styles.blueView}>
@@ -32,7 +199,7 @@ export default class huongdan extends React.Component{
                 </View>
                 <View style={styles.contentA}>
                     <Text style={styles.txtA}>Số câu</Text>
-                    <Text style={styles.txtB}>50</Text>
+                    <Text style={styles.txtB}>10</Text>
                 </View>
             </View>
         </View>
@@ -50,7 +217,7 @@ export default class huongdan extends React.Component{
             </View>
         </View>
         <View style={styles.vw4}>
-            <TouchableOpacity onPress={()=>navigation.navigate('testScreen',{baitap: id,n:1,ten:ten})} style={styles.btnStart}>
+            <TouchableOpacity onPress={()=>navigation.navigate('testScreen',{baitap: id,n:1,ten:ten,uid:uid,email:email,itemK:this.state.itemK,nameqs:this.state.nameqs,item:this.state.item,opt0:this.state.opt0,opt1:this.state.opt1,opt2:this.state.opt2,opt3:this.state.opt3,trueAns:this.state.trueAns})} style={styles.btnStart}>
                 <Text style={styles.txtStart}>Làm bài</Text>
             </TouchableOpacity>
         </View>
